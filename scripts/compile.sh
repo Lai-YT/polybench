13884e0118bd8a3f6ad8bc21f7435a6a2057a80e
Lai-YT <381xvmvbib@gmail.com>
2024-07-30 15:01:23 +0800

    chore: fix incorrect relative path
    The compile.sh is called by the runall.sh in the directory according to
    the benchmark we run; however, it assumes that it's always called under
    the project root, leading to incorrect relative path, causing
    compilation error.
    The path to the project root is now taken by the script as an argument.
    Additionally, description are added for each arguments in the help
    message.

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,17 +1,28 @@
 #!/bin/sh
 
-if [ $# -ne 3 ]; then
-    echo "Usage: compile.sh <compiler command> <input file> <output file>";
+if [ $# -ne 4 ]; then
+    echo "Usage: compile.sh <root dir> <compiler command> <input file> <output file>";
+    echo ""
+    echo "Arguments:"
+    echo "  <root dir>         The root directory of this benchmark;"
+    echo "                     since this script may be called from any directory,"
+    echo "                     it needs to know where the benchmark is located."
+    echo "  <compiler command> The command to use to compile the benchmark;"
+    echo "                     the instrumented code will be compiled automatically and"
+    echo "                     linked with the benchmark, as well as the libm library."
+    echo "  <input file>       The benchmark C file to compile."
+    echo "  <output file>      The name of the output executable to generate."
     exit 1;
 fi;
 
-COMPILER_COMMAND="$1";
-INPUT_FILE="$2";
-OUTPUT_FILE="$3";
+ROOT_DIR="$1";
+COMPILER_COMMAND="$2";
+INPUT_FILE="$3";
+OUTPUT_FILE="$4";
 
-$COMPILER_COMMAND  -lm -I utilities utilities/instrument.c $INPUT_FILE -o $OUTPUT_FILE
+$COMPILER_COMMAND  -lm -I $ROOT_DIR/utilities $ROOT_DIR/utilities/instrument.c $INPUT_FILE -o $OUTPUT_FILE
 
-echo "$COMPILER_COMMAND  -lm -I utilities utilities/instrument.c $INPUT_FILE -o $OUTPUT_FILE"
+echo "$COMPILER_COMMAND  -lm -I $ROOT_DIR/utilities $ROOT_DIR/utilities/instrument.c $INPUT_FILE -o $OUTPUT_FILE"
 
 
 exit 0;
