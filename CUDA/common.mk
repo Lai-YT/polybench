8703fd5f72bf945ac3a9014f236124a546e33050
Lai-YT <381xvmvbib@gmail.com>
2024-08-02 12:02:22 +0800

    chore(cuda): compile CUDA with clang
    This is to unify our benchmarking to always use clang as the driver.

--- CUDA/common.mk
+++ CUDA/common.mk
@@ -1,4 +1,14 @@
+ifeq ($(COMPILE_CUDA_WITH_CLANG),1)
+	CUDACXX=clang++-18
+	CUDAFLAGS=--cuda-gpu-arch="sm_86" -lcudart -lrt -pthread -ldl -L/usr/local/cuda/lib64
+else
+	CUDACXX=nvcc
+	CUDAFLAGS=
+endif
+
 all:
-	nvcc -O3 ${CUFILES} -o ${EXECUTABLE} 
+	$(CUDACXX) $(CUDAFLAGS) \
+		-O3 \
+		${CUFILES} -o ${EXECUTABLE} 
 clean:
 	rm -f *~ *.exe
