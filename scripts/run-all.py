3a02ff209873e354e86b18b4e33a984cd5812bf7
Lai-YT <381xvmvbib@gmail.com>
2024-08-04 14:32:02 +0800

    chore: adapt to Python script for better control

---
+++ scripts/run-all.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+
+import argparse
+import logging
+import subprocess
+import sys
+import tempfile
+from pathlib import Path
+
+BENCHMARKS = [
+    "linear-algebra/kernels",
+    "linear-algebra/solvers",
+    "datamining",
+    "image-processing",
+    "stencils",
+]
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        sys.argv[0],
+        description="A driver script to run the entire PolyBench/C benchmark suite.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "root", type=Path, metavar="ROOT", help="root directory of the benchmark suite"
+    )
+    parser.add_argument("-n", help="number of times to run each benchmark", default=5)
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=Path,
+        metavar="OUTPUT",
+        default=Path.cwd() / "results.csv",
+        help="output file to write the results to",
+    )
+    ppcg_group = parser.add_argument_group("PPCG benchmarks")
+    ppcg_group.add_argument("--ppcg", action="store_true", help="run PPCG benchmarks")
+    ppcg_group.add_argument(
+        "--ppcg-root",
+        type=Path,
+        metavar="PPCG_ROOT",
+        help="root directory of PPCG; only needed if --ppcg is specified",
+    )
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="enable verbose output"
+    )
+    args = parser.parse_args()
+
+    if args.verbose:
+        log_level = logging.DEBUG
+    else:
+        log_level = logging.INFO
+    logging.basicConfig(level=log_level, format="[%(levelname)s] %(message)s")
+
+    for benchmark in BENCHMARKS:
+        tmp = tempfile.mktemp()
+        if args.ppcg:
+            # First, run PPCG to generate the CUDA code.
+            for bench in (args.root / benchmark).iterdir():
+                source = bench / f"{bench.name}.c"
+                if not source.exists():
+                    logging.warning(f"skipping {source} as it does not exist")
+                    continue
+                compiler_opts = []
+                if (bench / "compiler.opts").is_file():
+                    compiler_opts = (bench / "compiler.opts").read_text().strip().split()
+                cmd = [
+                    f"{args.root}/scripts/ppcg-compile.py",
+                    args.ppcg_root,
+                    args.root,
+                    *compiler_opts,
+                    source,
+                ]
+                if args.verbose:
+                    cmd.append("-v")
+                # PPCG may be stuck in same cases, so we set a timeout.
+                try:
+                    ret = subprocess.run(cmd, timeout=60)
+                except subprocess.TimeoutExpired:
+                    logging.error(f"ppcg timed out for {source}")
+                    continue
+                if ret.returncode != 0:
+                    logging.error(f"ppcg failed to compile {source}")
+            # Then, run the generated CUDA code.
+            ret = subprocess.run(
+                [
+                    f"{args.root}/scripts/run-benchmark.py",
+                    benchmark,
+                    "-n",
+                    str(args.n),
+                    "-o",
+                    tmp,
+                    "--sort",
+                    "--dir",
+                    args.root,
+                    "--cuda",
+                    "--cuda-compiler",
+                    "clang++-18",
+                    "-v" if args.verbose else "--quiet",
+                ]
+            )
+        else:
+            ret = subprocess.run(
+                [
+                    f"{args.root}/scripts/run-benchmark.py",
+                    benchmark,
+                    "-n",
+                    str(args.n),
+                    "-o",
+                    tmp,
+                    "--sort",
+                    "--dir",
+                    args.root,
+                    "--compiler",
+                    "clang-18",
+                    "-v" if args.verbose else "--quiet",
+                ]
+            )
+        if ret.returncode == 0:
+            with args.output.open("a") as f:
+                f.write(open(tmp).read())
+        Path(tmp).unlink(missing_ok=True)
---
+++ scripts/run-benchmark.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python3
+
+import argparse
+import logging
+import subprocess
+import sys
+import tempfile
+from pathlib import Path
+
+POLYBENCH_DEFINES = ["-DPOLYBENCH_TIME", "-DDATA_TYPE=double"]
+
+
+class Runner:
+    def __init__(self, bench_cat: str, bench_root: Path, times: int):
+        self._bench_cat = bench_cat
+        self._bench_root = bench_root
+        self._times = times
+
+    def run(self) -> dict[str, list[float]]:
+        logging.info(f"Benchmark category: {self._bench_cat}")
+
+        bench_dir = self._bench_root / self._bench_cat
+        if not bench_dir.exists():
+            logging.error(f"directory {bench_dir} does not exist")
+            return {}
+
+        res: dict[str, list[float]] = {}
+        for bench in bench_dir.iterdir():
+            if sources := self._look_for_sources(bench):
+                logging.info(f"running benchmark {bench.name}")
+                output = tempfile.mktemp()
+                if self._compile(bench, sources, output) != 0:
+                    continue
+                res[bench.name] = self._execute(output)
+                if not res[bench.name]:
+                    # remove the benchmark from the results if it failed to execute
+                    del res[bench.name]
+                Path(output).unlink()
+            else:
+                logging.warning(f"no sources; skipping {bench.name}")
+        return res
+
+    # NOTE: Rely on args.
+    def _look_for_sources(self, bench: Path) -> list[Path]:
+        if args.cuda:
+            ext = ".cu"
+        else:
+            ext = ".c"
+        logging.info(f"looking for {ext} files")
+        return list(filter(lambda p: p.is_file(), bench.glob(f"*{ext}")))
+
+    def _compile(self, bench: Path, sources: list[Path], output: str) -> int:
+        """
+        Returns:
+            The return code of the compilation process.
+        """
+        compile_command = [
+            *self._compiler_command(),
+            "-O3",
+            *POLYBENCH_DEFINES,
+            "-lm",
+            "-I",
+            f"{self._bench_root}/utilities",
+            f"{self._bench_root}/utilities/instrument.c",
+            *list(map(str, sources)),
+            "-o",
+            output,
+        ]
+        if (bench / "compiler.opts").is_file():
+            compile_command.extend(
+                (bench / "compiler.opts").read_text().strip().split()
+            )
+        logging.debug(f"{' '.join(compile_command)}")
+        ret = subprocess.run(compile_command)
+        if ret.returncode != 0:
+            logging.error(f"failed to compile {list(map(str, sources))}")
+        return ret.returncode
+
+    # NOTE: Rely on args.
+    def _compiler_command(self) -> list[str]:
+        if args.cuda:
+            if args.cuda_compiler.startswith("nvcc"):
+                return [args.cuda_compiler, f"-arch={args.cuda_gpu_arch}"]
+            return [
+                args.cuda_compiler,
+                "-lcudart",
+                "-lrt",
+                "-ldl",
+                "-L/usr/local/cuda/lib64",
+                f"--cuda-gpu-arch={args.cuda_gpu_arch}",
+            ]
+        return [args.compiler]
+
+    def _execute(self, executable: str) -> list[float]:
+        res = []
+        for _ in range(self._times):
+            ret = subprocess.run([executable], capture_output=True, text=True)
+            if ret.returncode != 0:
+                logging.error(f"failed to execute {executable}")
+                return []
+            res.append(float(ret.stdout))
+            logging.info(f"{res[-1]}")
+        return res
+
+
+def write_results(output: Path, res: dict[str, list[float]]) -> None:
+    logging.info(f"writing results to {output}")
+    output.touch()
+    with output.open("w") as f:
+        for bench, times in res.items():
+            f.write(f"{bench},{','.join(map(str, times))}\n")
+
+
+BENCHMARKS = [
+    "linear-algebra/kernels",
+    "linear-algebra/solvers",
+    "datamining",
+    "stencils",
+    "image-processing",
+]
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        sys.argv[0],
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description="Run a Polybench benchmark and record the results in a CSV file.",
+    )
+    parser.add_argument(
+        "-n", type=int, default=5, help="number of times to run each benchmark"
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=Path,
+        default=Path.cwd() / "results.csv",
+        help="output file to write the results to",
+    )
+    parser.add_argument("-s", "--sort", action="store_true", help="sort the N results")
+    parser.add_argument(
+        "-d",
+        "--dir",
+        type=Path,
+        default=Path.cwd(),
+        help="the directory to the polybench folder",
+    )
+    parser.add_argument("-v", "--verbose", action="store_true")
+    parser.add_argument(
+        "-q", "--quiet", action="store_true", help="suppress output; overrides -v"
+    )
+
+    def check_compiler(compiler: str) -> str:
+        if compiler.startswith("clang") or compiler.startswith("gcc"):
+            return compiler
+        raise argparse.ArgumentTypeError("compiler must be clang* or gcc*")
+
+    parser.add_argument(
+        "--compiler", default="clang-18", help="clang* or gcc*", type=check_compiler
+    )
+    parser.add_argument(
+        "benchmark",
+        metavar="BENCHMARK_CATEGORY",
+        type=str,
+        choices=BENCHMARKS,
+        help=str(BENCHMARKS),
+    )
+
+    cuda_group = parser.add_argument_group("CUDA benchmarks")
+    cuda_group.add_argument(
+        "--cuda", action="store_true", help="look for CUDA files instead of C files"
+    )
+
+    def check_cuda_compiler(cuda_compiler: str) -> str:
+        if cuda_compiler.startswith("clang++") or cuda_compiler.startswith("nvcc"):
+            return cuda_compiler
+        raise argparse.ArgumentTypeError("cuda-compiler must be clang++* or nvcc*")
+
+    cuda_group.add_argument(
+        "--cuda-compiler",
+        type=str,
+        default="clang++-18",
+        help="clang++* or nvcc*; used if --cuda",
+    )
+    cuda_group.add_argument(
+        "--cuda-gpu-arch",
+        type=str,
+        default="sm_86",
+        help="the architecture of the GPU; used if --cuda",
+    )
+
+    args = parser.parse_args()
+
+    if args.quiet:
+        log_level = logging.ERROR
+    elif args.verbose:
+        log_level = logging.DEBUG
+    else:
+        log_level = logging.INFO
+    logging.basicConfig(level=log_level, format="[%(levelname)s] %(message)s")
+
+    runner = Runner(args.benchmark, args.dir, args.n)
+    res: dict[str, list[float]] = runner.run()
+    if args.sort:
+        logging.info("sorting results")
+        for times in res.values():
+            times.sort()
+
+    write_results(args.output, res)
